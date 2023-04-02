19dfac6914e02a2d782a37118872b8d3404ac7ec
Strifexss <rocombole0007@gmail.com>
2023-04-02 18:16:14 -0300

    #

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+var bodyParser = require('body-parser')
 
 app.use(cors())
+app.use(express.json())
 
 require('dotenv').config()
 const mysql = require('mysql2')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 console.log('Connected to PlanetScale!')
 
-app.get("/pesquisar", (req, res) => {
-    connection.query("Select * from pessoas;", (err, result) => {
+app.post("/registrar", (req, res, next) => {
+    const email = req.body.email
+    const nome = req.body.nome
+    const senha = req.body.senha
+
+    connection.query(`Insert into login (email, nome, senha) values ("${email}", "${nome}", "${senha}");`, (err, result) => {
+        res.send(err)
         res.send(result)
         console.log(err)
     })
 })
 
-app.post("/postar", (req, res) => {
-    connection.query("Insert into pessoas (pessoas_id, nome) values (3, 'Giovanny');", (err, result) => {
+app.use(express.json())
+
+app.post("/login", (req, res) => {
+    const email = req.body.email
+
+    connection.query(`Select * from login where email="${email}";`, (err, result) => {
         res.send(result)
         console.log(err)
+        console.log(result)
     })
+
 })
+
 
 app.listen(3001, () => {
     console.log("Servidor Inciado")
