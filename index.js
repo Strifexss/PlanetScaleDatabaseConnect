c8ddfb637eceaaf1f319bffd8dec99158e224cf9
Strifexss <rocombole0007@gmail.com>
2023-04-02 18:42:49 -0300

    #

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
-app.use(express.json())
 
 require('dotenv').config()
 const mysql = require('mysql2')
