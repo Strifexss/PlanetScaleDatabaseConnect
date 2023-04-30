6d4d8c9ecac108fbe237743c66c24f9a323b2b0d
Strifexss <rocombole0007@gmail.com>
2023-04-30 00:24:17 -0300

    #

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors(
-    {
-        origin: "*"
-    }
-));
+app.use(cors());
 //asd
 app.use(express.json())
 
