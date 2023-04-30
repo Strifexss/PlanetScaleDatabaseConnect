d410459a0d4cc71da6691cd126957c8e9417bbd5
Strifexss <rocombole0007@gmail.com>
2023-04-30 00:27:31 -0300

    #

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const connection = mysql.createConnection(process.env.DATABASE_URL)
 console.log('Connected to PlanetScale!')
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+  });
+
 app.get("/teste", (req, res) => {
     res.send("funciona")
 })
